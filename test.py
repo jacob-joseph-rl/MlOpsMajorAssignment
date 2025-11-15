a09fc0908c8ac68f6656f0618887e78bef555dd7
jacob-joseph-rl <jacob.joseph@gannett.com>
2025-11-15 19:31:23 +0530

    test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import joblib
-
-model = joblib.load('model.joblib')
-X_test, y_test = joblib.load('test_data.joblib')
-prediction = model.predict(X_test)
-score = model.score(X_test, y_test)
-print(f"Model Accuracy on test data: {round(score, 2)}")
